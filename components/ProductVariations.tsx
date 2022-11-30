60fca7c1adbc9e2d9bceb6ee777363c570a2018a
loisishere <lois.mitchom@gmail.com>
2022-11-30 15:42:21 -0500

    completion: finished assessment

--- components/ProductVariations.tsx
+++ components/ProductVariations.tsx
@@ -1,10 +1,34 @@
 /* eslint-disable @next/next/no-img-element */
 
-export default function ProductVariations() {
+type ProductVariationsProps = {
+  size?:[{
+    variation_value: string
+  }];
+  finish?: [{
+    variation_value: string
+  }];
+};
+
+export default function ProductVariations({size, finish}: ProductVariationsProps) {
   return (
     <div className="variations">
       <div className="size">Size</div>
+      <ul>
+      {size?.map((item, index)=>
+      (<li key={index}>
+        {item.variation_value}
+      </li>)
+      )}
+      </ul>
+      <hr/>
       <div className="finish">Finish</div>
+      <ul>
+      {finish?.map((item, index)=>
+      (<li key={index}>
+        {item.variation_value}
+      </li>)
+      )}
+      </ul>
     </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import type { NextPage } from "next";
 import Head from "next/head";
 import ProductDetails from "components/ProductDetails";
@@ -5,7 +6,29 @@ import ProductVariations from "components/ProductVariations";
 
 const title = "RTG Frontend technical assessment";
 
-const PDP: NextPage = () => (
+const PDP: NextPage = () => {
+  const [res, setRes] = useState(Object);
+  const [err, setErr] = useState("");
+  const [loading, setLoading] = useState(true);
+  const response = async () => {
+    try{
+      const fet = await fetch('/api/product')
+      const json = await fet.json();
+      if(json.title && json.primary_image && json.sku){
+        setRes(json);
+      }else{
+        setErr(json.errorMessage)
+      }
+      setLoading(false)
+    }catch(error){
+      setErr(error.errorMessage)
+    }
+  };
+  useEffect(() =>{
+    response();
+  },[])
+  
+  return (
   <div className="container">
     <Head>
       <title>{title}</title>
@@ -14,12 +37,19 @@ const PDP: NextPage = () => (
 
     <main className="main">
       <h1>{title}</h1>
-      <section className="product">
-        <ProductDetails />
-        <ProductVariations />
+      { loading ? 
+      <section>Loading... </section> :
+      err !== "" ?
+      <section>
+        {err}
       </section>
+      :
+      <section className="product">
+        <ProductDetails title={res?.title} primary_image={res?.primary_image} sku={res?.sku} />
+        <ProductVariations size={res?.variations?.size} finish={res?.variations?.finish} />
+      </section> }
     </main>
   </div>
-);
+)};
 
 export default PDP;
